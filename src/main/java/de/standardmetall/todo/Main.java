444d68199f165b305c146e88c3b5dbfc828167ac
rosenstein <rosenstein@DESKTOP-8CEC3SG.server.lan>
2022-04-06 14:32:09 +0200

    Changed from txt to json

--- src/main/java/de/standardmetall/todo/Main.java
+++ src/main/java/de/standardmetall/todo/Main.java
@@ -3,6 +3,7 @@ package de.standardmetall.todo;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
@@ -11,12 +12,17 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Main {
 	private static Scanner scanner = new Scanner(System.in);
@@ -25,19 +31,19 @@ public class Main {
 
 	public static void main(String args[]) throws InterruptedException {
 		System.out.println("--- TODO ---");
-		for(String string : args) {
-			if(string.equalsIgnoreCase("-disableAutoSave")) {
+		for (String string : args) {
+			if (string.equalsIgnoreCase("-disableAutoSave")) {
 				autosave = false;
 			}
-			if(string.equalsIgnoreCase("-debug")) {
+			if (string.equalsIgnoreCase("-debug")) {
 				debug = true;
 			}
 		}
 		loadTodo();
 		mainLoop();
-	}	
-	
-	//Main process loop
+	}
+
+	// Main process loop
 	private static void mainLoop() {
 		while (true) {
 			clearConsole(false);
@@ -71,13 +77,14 @@ public class Main {
 			}
 		}
 	}
-	//Console clearen
+
+	// Console clearen
 	private static void clearConsole(boolean delay) {
-		if(debug) {
+		if (debug) {
 			return;
 		}
 		try {
-			if(delay == true) {
+			if (delay == true) {
 				Thread.sleep(1500);
 			}
 			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
@@ -87,8 +94,8 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
-	//selbe wie oben nur für Leute die zu fault für argumente sind
+
+	// selbe wie oben nur für Leute die zu fault für argumente sind
 	private static void clearConsole() {
 		clearConsole(true);
 	}
@@ -103,18 +110,21 @@ public class Main {
 		} else {
 			System.out.println(" ");
 			for (int i = 0; i < Todo.getTasks().size(); i++) {
-				if(Todo.getTasks().get(i).getDate().compareTo(Calendar.getInstance().getTime()) > 0) {
-					System.out.println("-> " + (i + 1) + ") [" + DateFormat.getInstance().format(Todo.getTasks().get(i).getDate()) + "] " + Todo.getTasks().get(i).getDescription());
-				}
-				else {
-					System.out.println("-> " + (i + 1) + ") [\u001B[31m" + DateFormat.getInstance().format(Todo.getTasks().get(i).getDate()) + "\u001B[0m] " + Todo.getTasks().get(i).getDescription());
+				if (Todo.getTasks().get(i).getDate().compareTo(Calendar.getInstance().getTime()) > 0) {
+					System.out.println(
+							"-> " + (i + 1) + ") [" + DateFormat.getInstance().format(Todo.getTasks().get(i).getDate())
+									+ "] " + Todo.getTasks().get(i).getDescription());
+				} else {
+					System.out.println(
+							"-> " + (i + 1) + ") *[" + DateFormat.getInstance().format(Todo.getTasks().get(i).getDate())
+									+ "] " + Todo.getTasks().get(i).getDescription());
 				}
 			}
 			System.out.println(" ");
 		}
 	}
-	
-	//Sortiert Liste mit Todos nach datum neuste -> älteste
+
+	// Sortiert Liste mit Todos nach datum neuste -> älteste
 	private static void sortTasks() {
 		ArrayList<Todo> tasks = new ArrayList<>(Todo.getTasks());
 		Collections.sort(tasks);
@@ -128,32 +138,39 @@ public class Main {
 		System.out.println("B : Aufgabe hinzufügen");
 		System.out.println("C : Aufgabe entfernen");
 		System.out.println("D : Alle Aufgaben löschen");
-		if(!autosave) {
+		if (!autosave) {
 			System.out.println("E : Aufgaben speichern");
 		}
 		System.out.println(" ");
 	}
-	
-	//Fragt nach einer weiteren Aufgabe und fügt sie hinzu
+
+	// Fragt nach einer weiteren Aufgabe und fügt sie hinzu
 	private static void askForTaskAndAdd() {
 		System.out.println(" ");
 		System.out.println("Wie lautet die Aufgabe?");
 		String input = scanner.nextLine();
-		if(input == null) {
+		if (input == null) {
 			System.out.println("Bitte füge eine Beschreibung hinzu!");
 			askForTaskAndAdd();
 			return;
 		}
-		if(input.chars().allMatch(Character::isWhitespace)|| input.isEmpty()) {
+		if (input.chars().allMatch(Character::isWhitespace) || input.isEmpty()) {
 			System.out.println("Bitte füge eine Beschreibung hinzu!");
 			askForTaskAndAdd();
 			return;
 		}
-		System.out.println("Bis wann soll die Aufgabe erledigt sein?");
+		System.out.println("Bis wann soll die Aufgabe erledigt sein? (dd.mm.yyyy MM:HH)");
 		String dateInput = scanner.nextLine();
 		Date date = parseDate(dateInput);
-		if(date.compareTo(Calendar.getInstance().getTime()) <= 0) {
-			System.out.println("Das Datum muss in der Zukunft liegen!");
+		if (date == null) {
+			System.out.println("Bitte gibt das Datum wie folgt ein: dd.mm.yyyy mm.hh");
+			clearConsole();
+			printTasks();
+			askForTaskAndAdd();
+			return;
+		}
+		if (date.compareTo(Calendar.getInstance().getTime()) <= 0) {
+			System.out.println("Datum muss in der Zukunft liegen!");
 			clearConsole(true);
 			printTasks();
 			askForTaskAndAdd();
@@ -163,20 +180,18 @@ public class Main {
 		Todo.getTasks().add(todo);
 		System.out.println("Aufgabe erfolgreich hinzugefügt!");
 	}
-	
-	//Datum string zu datum
+
+	// Datum string zu datum
 	private static Date parseDate(String string) {
-		Date date = Calendar.getInstance().getTime();
+		// LocalDate date = null;
 		try {
-			date = DateFormat.getInstance().parse(string);
+			return new SimpleDateFormat("dd.MM.yyyy HH:mm").parse(string);
 		} catch (ParseException e) {
-			System.out.println("Bit gibt das Datum wie folgt ein: dd.mm.yyyy mm.hh");
-			return date;
+			return null;
 		}
-		return date;
 	}
-	
-	//Wartet auf eingabe von Aufgabennummer und löscht anschließend
+
+	// Wartet auf eingabe von Aufgabennummer und löscht anschließend
 	private static void askForTaskAndDelete() {
 		System.out.println(" ");
 		System.out.println("Welche Aufgabe solle gelöscht werden?");
@@ -187,11 +202,11 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println("Bitte gib eine Zahl an!");
 		}
-		if(task == -1) {
+		if (task == -1) {
 			askForTaskAndDelete();
 			return;
 		}
-		if(task <= 0 || task >= (Todo.getTasks().size() + 1)) {
+		if (task <= 0 || task >= (Todo.getTasks().size() + 1)) {
 			System.out.println("Bitte gib eine zahl von 1 bis " + (Todo.getTasks().size()));
 			askForTaskAndDelete();
 			return;
@@ -200,15 +215,15 @@ public class Main {
 		Todo.getTasks().remove(task);
 		System.out.println("Die Aufgabe wurde entfernt");
 	}
-	
-	//Löscht alle aufgaben
+
+	// Löscht alle aufgaben
 	private static void askForDeleteAll() {
 		System.out.println(" ");
 		System.out.println("Möchtest du wirklich fortfahren? (Ja|Nein)");
 		String input = scanner.nextLine();
-		if(input.equalsIgnoreCase("Ja")) {
+		if (input.equalsIgnoreCase("Ja")) {
 			ArrayList<Todo> tasks = new ArrayList<Todo>(Todo.getTasks());
-			for(Todo todo: tasks) {
+			for (Todo todo : tasks) {
 				Todo.getTasks().remove(todo);
 			}
 			System.out.println("Alle Aufgaben gelöscht!");
@@ -217,48 +232,57 @@ public class Main {
 		}
 		System.out.println("Löschen abgebrochen");
 	}
-	
-	//Speichert Todo in todo.txt
+
+	// Speichert Todo in todo.txt
+	@SuppressWarnings("unchecked")
 	private static void saveTodo() {
-		File file = new File("todo.txt");
-		if(file.exists()) {
-			file.delete();
+		JSONArray jsonObject = new JSONArray();
+		for (int i = 0; i < Todo.getTasks().size(); i++) {
+			JSONObject json = new JSONObject();
+			json.put("Date", DateFormat.getInstance().format(Todo.getTasks().get(i).getDate()));
+			json.put("description", Todo.getTasks().get(i).getDescription());
+			jsonObject.add(json);
 		}
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			for(Todo todo : Todo.getTasks()) {
-				writer.write(DateFormat.getInstance().format(todo.getDate()) + ";" +todo.getDescription());
-				writer.newLine();
+			File file = new File("todo.json");
+			if (file.exists()) {
+				file.delete();
 			}
+			FileWriter writer = new FileWriter(file);
+			writer.write(jsonObject.toJSONString());
+			writer.flush();
 			writer.close();
-		} catch (IOException e) {
-			System.out.println("Todoliste konnte nicht gespeichert werden");
+		} catch (Exception e) {
+			System.out.println("Fehler beim Speichern");
 		}
 	}
-	
-	//Lädt todo aus todo.txt
+
+	// Lädt todo aus todo.txt
 	private static void loadTodo() {
-		File file = new File("todo.txt");
+		File file = new File("todo.json");
+		if (!file.exists()) {
+			return;
+		}
 		try {
-			URI uri = file.toURI();
-			List<String> lines = Files.readAllLines(Paths.get(uri), StandardCharsets.UTF_8);
-			for(String string : lines) {
-				Date date = DateFormat.getInstance().parse(string.split(";")[0]);
-				Todo.getTasks().add(new Todo(string.split(";", 2)[1], date));
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(new FileReader(file));
+			JSONArray jsonArray = (JSONArray) obj;
+			for(Object o : jsonArray) {
+				JSONObject jo = (JSONObject) o;
+				String date = (String) jo.get("Date");
+				String description = (String) jo.get("description");
+				System.out.println(date + " " + description);
+				Todo.getTasks().add(new Todo(description, DateFormat.getInstance().parse(date)));
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Todoliste konnte nicht geladen werden " + e.getLocalizedMessage());
-		} catch (IOException e) {
-			System.out.println("Todoliste konnte nicht geladen werden " + e.getLocalizedMessage());
-		} catch (ParseException e) {
-			System.out.println("Todoliste konnte nicht geladen werden " + e.getLocalizedMessage());
+		} catch (Exception e) {
+			System.out.println("Fehler beim laden");
 		}
 	}
-	
-	//Überprüft ob autosave an ist und speichert
+
+	// Überprüft ob autosave an ist und speichert
 	private static void autoSave() {
-		if(autosave) {
-		saveTodo();
+		if (autosave) {
+			saveTodo();
 		}
 	}
 
